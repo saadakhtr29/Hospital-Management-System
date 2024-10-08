c2d171cca3bf36a59f6582b25d74a3ff0e71cc68
saadakhtr29 <saadakhtr51@gmail.com>
2024-10-08 06:17:34 +0530

    project update

---
+++ Backend/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookiesParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+export default app;
