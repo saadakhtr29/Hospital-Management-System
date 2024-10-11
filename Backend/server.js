e6454750f24318ccca07a33b395a8d77266b21d7
saadakhtr29 <saadakhtr51@gmail.com>
2024-10-11 23:04:52 +0530

    project update

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,12 @@
 import app from "./app.js";
+import cloudinary from "cloudinary";
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_API_KEY,
+  cloud_name: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
