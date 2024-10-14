165eb93ff1f2664f4196706b8e4e980bc054992a
saadakhtr29 <saadakhtr51@gmail.com>
2024-10-15 02:32:58 +0530

    project update

--- Backend/controller/messageController.js
+++ Backend/controller/messageController.js
@@ -1,6 +1,7 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import { Message } from "../models/messageSchema.js";
 
-export const sendMessage = async (req, res, next) => {
+export const sendMessage = catchAsyncErrors(async (req, res, next) => {
   const { firstName, lastName, email, phone, message } = req.body;
   if (!firstName || !lastName || !email || !phone || !message) {
     return res.status(400).json({
@@ -13,4 +14,4 @@ export const sendMessage = async (req, res, next) => {
     success: true,
     message: "Message send successfully",
   });
-};
+});
