8f2bfeb59599a0041d70fc93e33aa6e2d2007341
saadakhtr29 <saadakhtr51@gmail.com>
2025-09-26 07:51:30 +0530

    Completed the Project: Completed the backend, added the  frontend and dashboard for the project

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { config } from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import messageRouter from "./router/messageRouter.js";
-import {errorMiddleware} from "./middlewares/errorMiddleware.js"
 import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -14,8 +15,8 @@ config({ path: "./config/config.env" });
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // allows cookies/auth headers
   })
 );
 
@@ -29,12 +30,11 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
-
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
 
-
-dbConnection()
+dbConnection();
 
 app.use(errorMiddleware);
 export default app;
